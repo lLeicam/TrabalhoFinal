38970a053db3213f71dffa0fb1c890030acec6bc
vitoriavivian227@gmail.com <maciel.vivian@alunoescolassociais.org.br>
2021-09-22 17:25:43 -0300

    finalização

--- php/cadastrar.php
+++ php/cadastrar.php
@@ -14,16 +14,16 @@ $cep            = $_POST["cep"];
 $uf             = $_POST["uf"];
 $cidade         = $_POST["cidade"];
 $logradouro     = $_POST["logradouro"];
-$numero         = $_POST[
+$numero         = $_POST["numero"];
 $complemento    = $_POST["complemento"];
 
 
-$cadastra = "INSERT INTO infousers (nome, genero, user, email, senha, date, cpf, telefone, cep, uf, cidade, logradouro, numero, complemento) VALUES ('$nome', '$genero', '$user', '$email', '$senha', '$date', '$cpf', '$telefone', '$cep', '$uf', '$cidade', '$logradouro', '$numero', '$complemento')";
+$cadastra = "INSERT INTO infousers (nome, genero, user, email, senha, data, cpf, telefone, cep, uf, cidade, logradouro, numero, complemento) VALUES ('$nome', '$genero', '$user', '$email', '$senha', '$date', '$cpf', '$telefone', '$cep', '$uf', '$cidade', '$logradouro', '$numero', '$complemento')";
 
 if (mysqli_query($conn, $cadastra)){
     echo "<h1>Novo cadastro realizado</h1></br>";
 } else {
-    echo "Erro: " . $cadastra . "<br>" . mysqli_connect_error($conn);
+    echo "Erro: " . $cadastra . "<br>" . mysqli_error($conn);
 }
 
 mysqli_close($conn);
--- php/conexao.php
+++ php/conexao.php
@@ -8,7 +8,7 @@ $dbname     = "formulario";
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
 if (!$conn){
-    die("Falha de conexão: " . mysql_connect_error());
+    die("Falha de conexão: " . mysqli_error());
 }
 
 echo "<h1>Conexão bem sucedida</h1>";
